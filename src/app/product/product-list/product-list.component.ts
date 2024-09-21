d30e6ca2ff38ace4f89afd75d2108dfcc84b7e34
20000995 <20000995@alumnos.utleon.edu.mx>
2024-09-21 10:15:59 -0600

    Sabado 21 Ejemplo 1

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IProductos } from '../productos';
 
 @Component({
   selector: 'app-product-list',     /* Nombre del componente */
@@ -6,16 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
-
 //*ngIf=true, o false   Hace una accion si es verdadero o falso
 
-  productos:any[]=[
+imageWidth:number=60;
+imageMargin:number=2;
+muestraImg:boolean=true;
+listFilter:string=''
+
+muestImg():void{                  
+  this.muestraImg=!this.muestraImg  //operador ternario !
+}
+
+  productos:IProductos[]=[       //esto es type scrip | es un arreglo de objetos
     {
       "productoId":1,
       "Modelo":"Sentra",
       "Descripcion":"4 puertas",
       "Precio": 20000,
-      "Year":2023,
+      "Year":"febrero 2 2023",
       "Marca":"NISSAN",
       "Color":"Azul",
       "ImagenUrl":"https://acroadtrip.blob.core.windows.net/catalogo-imagenes/xl/RT_V_0c7166c16ff74073b65851f7f0b360ed.jpg"
@@ -25,7 +34,7 @@ export class ProductListComponent {
       "Modelo":"A4",
       "Descripcion":"4 puertas",
       "Precio": 30000,
-      "Year":2022,
+      "Year":"marzo 23 2022",
       "Marca":"AUDI",
       "Color":"Azul",
       "ImagenUrl":"https://acnews.blob.core.windows.net/imgnews/medium/NAZ_3a2049521d694131ac5fa4632c7d8325.jpg"
@@ -35,11 +44,10 @@ export class ProductListComponent {
       "Modelo":"Rio",
       "Descripcion":"4 puertas",
       "Precio": 150000,
-      "Year":2020,
+      "Year":"septiembre 5 2020",
       "Marca":"KIA",
       "Color":"Azul",
       "ImagenUrl":"https://cdn.buttercms.com/JATRezqBSzmBK1jOXk4q"
     }
   ]
-
 }
---
+++ src/app/product/productos.ts
@@ -0,0 +1,11 @@
+export interface IProductos {       //se creo la interfaz para el objeto
+    productoId:number;
+      Modelo:string;
+      Descripcion:string;
+      Precio: number;
+      Year:string;
+      Marca:string;
+      Color:string;
+      ImagenUrl:string;
+}
+
